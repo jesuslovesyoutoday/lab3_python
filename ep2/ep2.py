5560ae0911c2ea75d2ebb2510ad66f4eeb4ea456
jesuslovesyoutoday <jesuslovesyoutoday@yadex.ru>
2021-12-17 22:14:01 +0300

    replace loop

--- ep2/ep2.py
+++ ep2/ep2.py
@@ -3,26 +3,19 @@ import matplotlib.pyplot as plt
 import os
 
 def read_file(filename):
-    """with open(filename) as f:
-        data = np.array(f.read().split('\n'), dtype = np.float64)"""
     data = np.loadtxt(filename)
     return data
         
 def filter_signal(data):
-    new_data = [data[0]]
-    for i in range(1, len(data)):
-        lim = i - 9
-        if lim < 0:
-            lim = 0
-        new_data.append(np.mean(data[lim:i]))
+    new_data_0 = [data[0]]
+    new_data = [np.mean(data[(x-9):x]) if x-9 > 0 else np.mean(data[0:x]) for x in range (1, len(data))]
+    new_data = new_data_0 + new_data
     new_data = np.array(new_data)
     return new_data
     
 def graph(data, new_data):
     fig, axs = plt.subplots(2, 2)
     x = np.linspace(0, len(new_data), len(data), dtype = np.int64)
-    #print(x, len(x))
-    #print(data, new_data, len(data), len(new_data))
     
     plt.subplot(1, 2, 1)
     plt.plot(x, data)
@@ -38,7 +31,7 @@ def graph(data, new_data):
 
 files = next(os.walk("signals"))
 file_count = len(files)
-print(file_count)
+#print(file_count)
 
 for i in range(1, file_count + 1):
     filename = "signals/signal0" + str(i) + ".dat"
